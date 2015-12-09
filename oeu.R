9f32527b5f7438e7b5a9a5350a48e105b4f3e682
swo <swo@mit.edu>
2015-12-09 18:16:02 -0500

    small fixes

--- oeu.R
+++ oeu.R
@@ -4,8 +4,13 @@ library(dplyr)
 
 # global parameters
 k <- 50
+min.otu.size <- 1000
 prune.min.cor <- 0.75
 filter.min.size <- 2
+max.blank.frac <- 0.10
+max.sediment.frac <- 0.05
+top.otus <- 400
+
 out.fn <- "groups.dat"
 plot.dat.fn <- "plot.dat"
 
@@ -18,7 +23,7 @@ otu$M8 <- otu$M8.1 + otu$M8.2
 otu <- otu[, !(colnames(otu) %in% c("M3.1", "M3.2", "M8.1", "M8.2"))]
 
 # note which OTUs that have fewer than 1000 counts
-too.small.otus <- rowSums(otu) < 1000
+too.small.otus <- rowSums(otu) < min.otu.size
 
 # normalize by column (i.e., convert to relative abundances)
 otu <- apply(otu, 2, function(x) x / sum(x)) %>% as.data.frame
@@ -27,10 +32,10 @@ otu <- apply(otu, 2, function(x) x / sum(x)) %>% as.data.frame
 otu <- otu[!too.small.otus, ]
 
 # remove OTUs w/ more than 10% of their reads in the blanks
-otu <- otu[otu$MEB + otu$MSB <= 0.10, ]
+otu <- otu[otu$MEB + otu$MSB <= max.blank.frac, ]
 
 # remove OTUs w/ more than 5% of their reads in the sediment-y sample (M22)
-otu <- otu[otu$M22 <= 0.05, ]
+otu <- otu[otu$M22 <= max.sediment.frac, ]
 
 # remove extra columns
 otu <- otu[, !(colnames(otu) %in% c("MEB", "MSB", "M22"))]
@@ -39,9 +44,9 @@ otu <- otu[, !(colnames(otu) %in% c("MEB", "MSB", "M22"))]
 otu <- otu[order(rowSums(otu), decreasing=TRUE), ]
 
 # swo> there are 484 OTUs now
-# take the most abundant 300 OTUs only
+# take the most abundant OTUs only
 # otherwise, you get a funny drop-off in r.a. at ~475
-otu <- otu[1:300, ]
+otu <- otu[1:top.otus, ]
 
 # normalize the OTUs by row
 otu <- apply(otu, 1, function(x) x / sum(x)) %>% t %>% as.data.frame
